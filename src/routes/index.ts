f55a8c4fc547833ba22cbe0667927c9f37b2417b
maurolnl <mauroleoquiroga@gmail.com>
2020-08-10 17:36:56 -0300

    Added routes for User

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,22 @@
 import { Router } from "express";
 const router = Router();
 
-import { createPhoto, getPhotos, getPhotoById, deletePhotoById, updatePhotoById} from "../controllers/photo.controller";
+import { 
+  createPhoto,
+  getPhotos,
+  getPhotoById,
+  deletePhotoById,
+  updatePhotoById
+} from "../controllers/photo.controller";
+
+import {
+  createUser,
+  getUsers,
+  updateUserById,
+  deleteUser,
+  getUserById
+} from "../controllers/user.controller";
+
 import multer from "../libs/multer";
 
 router.route('/photos')
@@ -12,5 +27,14 @@ router.route('/photos/:id')
   .get(getPhotoById)
   .delete(deletePhotoById)
   .put(updatePhotoById)
+
+  router.route('/users')
+    .post(createUser)
+    .get(getUsers)
+
+  router.route('/users/:id')
+    .put(updateUserById)    
+    .delete(deleteUser)
+    .get(getUserById)
 
 export default router;
